389ac1639c912ee25402d7d3095d58ef09e55fc4
Sumit Rakshit <sraksh@sapient.com>
2017-07-14 10:55:33 +0530

    added reducers, webpack config, babel

---
+++ dev/js/reducers/reducer_books.js
@@ -0,0 +1,15 @@
+export default function () {
+  return [{
+      title: 'Javascript: Good Parts'
+    },
+    {
+      title: 'Harry Potter'
+    },
+    {
+      title: 'The Dark Tower'
+    },
+    {
+      title: 'Five point someone'
+    }
+  ]
+}
